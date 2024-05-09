befc5c26db6e6050f35437c31c8e91f4212bdad5
Derek <derekeklund32@gmail.com>
2024-05-09 08:18:28 -0400

    Small update to return for index

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return "Let's try this again!"
 
 if __name__ == '__main__':
     app.run(debug=True) # This should be set to false in a production environment
